caeac111d456a0a1fd23cf879998d952950c3a7f
Tommaso Bianucci <tommaso@philae>
2018-05-05 14:17:34 +0200

    Added types infrastructure for supporting outer boundary conditions.

--- WS2/Project/boundary_val.h
+++ WS2/Project/boundary_val.h
@@ -1,10 +1,29 @@
 #ifndef __RANDWERTE_H__
 #define __RANDWERTE_H__
 
+/*
+ * Auxiliary data structures to handle the boundary values.
+ */
+typedef enum BoundaryType
+{
+    DIRICHLET,
+    NEUMANN,
+    SAME
+} BoundaryType;
+
+typedef struct BoundaryInfo
+{
+    BoundaryType typeU;
+    BoundaryType typeV;
+    char constU; // 1 means the value to apply is uniform.
+    char constV; // 1 means the value to apply is uniform.
+    double* valuesU;
+    double* valuesV;
+} BoundaryInfo;
 
 /**
  * The boundary values of the problem are set.
  */
-void boundaryvalues(int imax, int jmax, double **U, double **V, int **Flags);
+void boundaryvalues(int imax, int jmax, double **U, double **V, int **Flags, BoundaryInfo boundaryInfo[4]);
 
 #endif
