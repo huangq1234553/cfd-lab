0777fe061133b20d9363280ed14c0d6e151f2827
Tommaso Bianucci <tommaso@philae>
2018-05-06 20:08:37 +0200

    New script create-geometry.sh allows to convert b/w images to a valid geometry file.

---
+++ WS2/Project/create-geometry.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+### Convert b/w image files (as e.g. a jpeg) to a .pgm file suitable to be used as geometry
+# Note: forbidden geometries will NOT be checked/corrected! Also there is no guarantee
+#   that rescaling will not introduce any forbidden geometry!
+# Rescaling will be performed if optional width and height arguments are given
+#
+# Before usage you might need to make this file executable with:
+#   chmod +x create-geometry.sh
+#
+# Usage:
+#   ./create-geometry.sh FILE.jpg [width height]
+#
+
+USAGE="Usage:\n\t./create-geometry.sh FILE [width height]"
+CONVERT_FLAGS="-posterize 2 -depth 1 -compress none -negate"
+
+if [[ ! $1 ]]; then
+    echo "ERROR: You need to pass a jpg file as argument!"
+    echo -e ${USAGE}
+    exit 1
+fi
+
+INFILE="$1"
+BASENAME=${INFILE%.*}
+OUTFILE=${BASENAME}.pgm
+
+if [[ $2 && $3 ]]; then
+    WIDTH=$2
+    HEIGHT=$3
+    echo "INFO: rescaling the geometry to ${WIDTH}x${HEIGHT}"
+    CONVERT_FLAGS="-scale ${WIDTH}x${HEIGHT} ${CONVERT_FLAGS}"
+fi
+
+echo "INFO: generating ${OUTFILE}"
+convert ${CONVERT_FLAGS} ${INFILE} ${OUTFILE}; sed -i s/"255"/"1"/g ${OUTFILE}
+exit $?
+
+#eof
