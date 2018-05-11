9718565bb4131352a619df6491755599b2a84259
Tommaso Bianucci <tommaso@philae>
2018-05-11 09:01:47 +0200

    Added --notemp switch to prevent temp computations if we don't need it. Also good defaults for Re and Pr (1).

--- WS2/Project/init.c
+++ WS2/Project/init.c
@@ -22,6 +22,8 @@ int read_parameters(const char *szFileName, double *Re, double *UI, double *VI, 
     READ_DOUBLE(szFileName, *ylength, REQUIRED);
     
     READ_DOUBLE(szFileName, *Re, REQUIRED);
+    if (*Re == 0) // Re cannot be 0, setting a sane default
+        *Re = 1.0;
     READ_DOUBLE(szFileName, *t_end, REQUIRED);
     READ_DOUBLE(szFileName, *dt, REQUIRED);
     
@@ -47,6 +49,8 @@ int read_parameters(const char *szFileName, double *Re, double *UI, double *VI, 
 //    READ_DOUBLE(szFileName, *T_h, OPTIONAL);  // NOT REQUIRED
 //    READ_DOUBLE(szFileName, *T_c, OPTIONAL);  // NOT REQUIRED
     READ_DOUBLE(szFileName, *Pr, OPTIONAL);
+    if (*Pr == 0) // Pr cannot be 0, setting a sane default
+        *Pr = 1.0;
     
     READ_STRING(szFileName, problem, REQUIRED);
     READ_STRING(szFileName, geometry, REQUIRED);
