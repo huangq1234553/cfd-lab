98289cbe6af1842e17f3ea6b0d31fd34a15a740f
Tommaso Bianucci <tommaso@philae>
2018-05-22 13:27:54 +0200

    First step toward a functioning visualization output.

--- WS2/Project/main.c
+++ WS2/Project/main.c
@@ -7,6 +7,7 @@
 // #include "logger.h"
 #include <mpi.h>
 #include "parallel.h"
+#include "logger.h"
 
 
 /**
@@ -43,16 +44,17 @@
  * - calculate_uv() Calculate the velocity at the next time step.
  */
 
-int main(int argc, char** argv){
-	
+int main(int argc, char **argv)
+{
+    
     // Handling the problem file name which is passed as 1st argument.
-	char szFileName[256]; // We assume name will not be longer than 256 chars...
+    char szFileName[256]; // We assume name will not be longer than 256 chars...
     strcpy(szFileName, argv[1]);
     strcat(szFileName, ".dat");
     //
-	char problem[256];
+    char problem[256];
     char geometry[1024]; // bigger since this can be a full path
-	double Re;                /* reynolds number   */
+    double Re;                /* reynolds number   */
     double UI;                /* velocity x-direction */
     double VI;                /* velocity y-direction */
     double PI;                /* pressure */
@@ -64,79 +66,80 @@ int main(int argc, char** argv){
     double dt;                /* time step */
     double dx;                /* length of a cell x-dir. */
     double dy;                /* length of a cell y-dir. */
-    int  imax;                /* number of cells x-direction*/
-    int  jmax;                /* number of cells y-direction*/
+    int imax;                /* number of cells x-direction*/
+    int jmax;                /* number of cells y-direction*/
     double alpha;             /* uppwind differencing factor*/
     double omg;               /* relaxation factor */
     double tau;               /* safety factor for time step*/
-	int  itermax;			  /* max. number of iterations  */
+    int itermax;              /* max. number of iterations  */
     double eps;               /* accuracy bound for pressure*/
     double dt_value;          /* time for output */
-	// int n = 0;				  /* timestep iteration counter */
-	double res = 10;		  /* residual */
-	// double t = 0;			  /* initial time */
-	int it;					  /* sor iteration counter */
+    int n = 0;                  /* timestep iteration counter */
+    double res = 10;          /* residual */
+    double t = 0;              /* initial time */
+    int it;                      /* sor iteration counter */
     int iproc;                  /* number of processes in i direction*/
     int jproc;                  /* number of processes in j direction*/
-	// double mindt=10000;
+    // double mindt=10000;
     
     // openLogFile(); // Initialize the log file descriptor.
     read_parameters(szFileName, &Re, &UI, &VI, &PI, &GX, &GY, &t_end, &xlength, &ylength, &dt, &dx, &dy, &imax, &jmax,
                     &alpha, &omg,
                     &tau, &itermax, &eps, &dt_value, problem, geometry, &iproc, &jproc);
+    strcpy(problem,argv[1]); // Just because we are not currently reading it from config. TODO: fix this properly.
+    
+    MPI_Status status;
+    int my_rank, num_proc;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    int il, ir, jb, jt;
+    int rank_l, rank_r, rank_b, rank_t;
+    int omg_i, omg_j;
+    
+    init_parallel(iproc, jproc, imax, jmax, my_rank, &il, &ir, &jb, &jt, &rank_l, &rank_r, &rank_b, &rank_t,
+                  &omg_i, &omg_j, num_proc);
+    
+    int imax_local = ir - il, jmax_local = jt - jb;
+    
+    
+    double **P = matrix(0, imax_local + 2, 0, jmax_local + 2);
+    double **U = matrix(0, imax_local + 3, 0, jmax_local + 2);
+    double **V = matrix(0, imax_local + 2, 0, jmax_local + 3);
+    double **F = matrix(0, imax_local + 3, 0, jmax_local + 2);
+    double **G = matrix(0, imax_local + 2, 0, jmax_local + 3);
+    double **RS = matrix(1, imax_local + 1, 1, jmax_local + 1);
+    double *bufSend = (double *) malloc((size_t) (3 * (ir - il + 3) * sizeof(double *)));
+    double *bufRecv = (double *) malloc((size_t) (3 * (ir - il + 3) * sizeof(double *)));
+    
+    init_uvp(UI, VI, PI, imax_local, jmax_local, U, V, P);
 
-   MPI_Status status;
-   int my_rank, num_proc;
-   MPI_Init(&argc, &argv);
-   MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
-   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);   
-   int il, ir, jb, jt;
-   int rank_l, rank_r, rank_b, rank_t;
-   int omg_i, omg_j;
-
-   init_parallel ( iproc, jproc, imax, jmax, my_rank, &il, &ir, &jb, &jt, &rank_l, &rank_r, &rank_b, &rank_t,
-				&omg_i, &omg_j, num_proc);
-
-   int imax_local = ir - il, jmax_local = jt - jb;
-
-   
-
-   double** P = matrix( 0	, imax_local + 2,  0, jmax_local + 2);
-   double** U = matrix(	0 	, imax_local + 3,  0, jmax_local + 2);
-   double** V = matrix( 0 	, imax_local + 2,  0, jmax_local + 3);
-   double** F = matrix(	0	, imax_local + 3,  0, jmax_local + 2);
-   double** G = matrix(	0	, imax_local + 2,  0, jmax_local + 3);
-   double** RS = matrix(1	, imax_local + 1,  1, jmax_local + 1);
-   double* bufSend = (double*) malloc( (size_t)( 3 * (ir - il + 3) * sizeof(double*)) );
-   double* bufRecv = (double*) malloc( (size_t)( 3 * (ir - il + 3) * sizeof(double*)) );
-
-   init_uvp(UI, VI, PI, imax_local, jmax_local, U, V, P);
-   
 
 // 	// TODO: Check if this visualization output can be removed!
-
-
-	// write_vtkFile(problem, n, xlength, ylength, imax, jmax, dx, dy, U, V, P);
-	// n++;
+    
+    printf("[R%d] Right before writing the viz...\n", my_rank); //debug
+    write_vtkFile(problem, n, my_rank, xlength, ylength, imax_local, jmax_local, dx, dy, U, V, P);
+    n++;
+    printf("[R%d] Right after writing the viz...\n", my_rank); //debug
 // 	// simulation interval 0 to t_end
-	// double currentOutputTime = 0; // For chosing when to output
-	// while(t < t_end){
-		
-		// adaptive stepsize control based on stability conditions ensures stability of the method!
-		// dt = tau * min(cond1, cond2, cond3) where tau is a safety factor
-		// NOTE: if tau<0, stepsize is not adaptively computed!
-		// if(tau > 0){
-			// calculate_dt(Re, tau, &dt, dx, dy, imax, jmax, U, V);
-            // dt = fmin(dt, dt_value); // test, to avoid a dt bigger than visualization interval
-			// Used to check the minimum time-step for convergence
-			// if (dt < mindt)
-			// 	mindt = dt;
-		// }
-		
-		// ensure boundary conditions for velocity
-
-        // here we only need to set boundary values if local boundaries coincide with global boundaries
-    if (omg_i == 0 || omg_i == iproc - 1 || omg_j == 0 || omg_j == jproc -1)
+    // double currentOutputTime = 0; // For chosing when to output
+    // while(t < t_end){
+    
+    // adaptive stepsize control based on stability conditions ensures stability of the method!
+    // dt = tau * min(cond1, cond2, cond3) where tau is a safety factor
+    // NOTE: if tau<0, stepsize is not adaptively computed!
+    // if(tau > 0){
+    // calculate_dt(Re, tau, &dt, dx, dy, imax, jmax, U, V);
+    // dt = fmin(dt, dt_value); // test, to avoid a dt bigger than visualization interval
+    // Used to check the minimum time-step for convergence
+    // if (dt < mindt)
+    // 	mindt = dt;
+    // }
+    
+    // ensure boundary conditions for velocity
+    
+    // here we only need to set boundary values if local boundaries coincide with global boundaries
+    if (omg_i == 0 || omg_i == iproc - 1 || omg_j == 0 || omg_j == jproc - 1)
     {
         boundaryvalues(omg_i, omg_j, imax_local, jmax_local, U, V);
     }
@@ -145,6 +148,7 @@ int main(int argc, char** argv){
 //			write_vtkFile(problem, n, xlength, ylength, imax, jmax, dx, dy, U, V, P);
 //			n++;
 //		}
+
 		// momentum equations M1 and M2 - F and G are the terms arising from explicit Euler velocity update scheme
 		calculate_fg(Re, GX, GY, alpha, dt, dx, dy, imax_local, jmax_local, U, V, F, G);
 
@@ -193,27 +197,27 @@ int main(int argc, char** argv){
 // 		// advance in time
 // 		t += dt;
 // 	}
-
-// 	// write visualisation file for the last iteration
-//     logEvent(t, "INFO: Writing visualization file n=%d", n);
-//     write_vtkFile(problem, n, xlength, ylength, imax, jmax, dx, dy, U, V, P);
+    
+    // write visualisation file for the last iteration
+    logEvent(t, (char*)"INFO: Writing visualization file n=%d", n);
+    write_vtkFile(problem, n, 0, xlength, ylength, imax, jmax, dx, dy, U, V, P);
 
 // 	// Check value of U[imax/2][7*jmax/8] (task6)
 //     logMsg("Final value for U[imax/2][7*jmax/8] = %16e", U[imax / 2][7 * jmax / 8]);
-   	free_matrix(P, 	0	, imax_local + 1,  0, jmax_local + 1);
-   	free_matrix(U,	0 	, imax_local + 2,  0, jmax_local + 1);
-   	free_matrix(V,  0 	, imax_local + 1,  0, jmax_local + 2);
-   	free_matrix(F,	0	, imax_local + 2,  0, jmax_local + 1);
-   	free_matrix(G,	0	, imax_local + 1,  0, jmax_local + 2);
-   	free_matrix(RS, 1	, imax_local,  1, jmax_local);
-   	free( bufSend );
-   	free( bufRecv );
+    free_matrix(P, 0, imax_local + 1, 0, jmax_local + 1);
+    free_matrix(U, 0, imax_local + 2, 0, jmax_local + 1);
+    free_matrix(V, 0, imax_local + 1, 0, jmax_local + 2);
+    free_matrix(F, 0, imax_local + 2, 0, jmax_local + 1);
+    free_matrix(G, 0, imax_local + 1, 0, jmax_local + 2);
+    free_matrix(RS, 1, imax_local, 1, jmax_local);
+    free(bufSend);
+    free(bufRecv);
     
     MPI_Finalize();
     // logMsg("Min dt value used: %16e", mindt);
     
     // closeLogFile(); // Properly close the log file
-
-	return 0;
+    
+    return 0;
 }
 
